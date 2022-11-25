5b62adbf182761e44727b7a0830d67092fd83ac5
denervalspelizario <denervalspelizario@gmail.com>
2022-11-25 08:43:56 -0300

    update projeto agencia jsx Footer

--- agencia/src/componentes/Card.jsx
+++ agencia/src/componentes/Card.jsx
@@ -19,26 +19,26 @@ function Card() {
       </div>
       <div className='card__cards'>
         <p className='card__text--up'>
-          Junho 2012 -2016
+          Agosto 2016 -2019
         </p>
-        <h3>Web Designer</h3>
+        <h3>Product Designer</h3>
         <p className='card__text'>
-          Pied Piper StartUp
+          E Corp
         </p>
         <p className='card__text'>
-          Criação de Landing pages, sites institucionais e E-comemerces completamente personalizados e otimizados para buscadores
+          Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra, impactando-o no momento certo
         </p>
       </div>
       <div className='card__cards'>
         <p className='card__text--up'>
-          Junho 2012 -2016
+          Fevereiro 2019 - 2021
         </p>
-        <h3>Web Designer</h3>
+        <h3>Digital Consulting</h3>
         <p className='card__text'>
-          Pied Piper StartUp
+          Arasaka Inc
         </p>
         <p className='card__text'>
-          Criação de Landing pages, sites institucionais e E-comemerces completamente personalizados e otimizados para buscadores
+          Consultoria em estratégias digitais para todas as etapas do cliclo do projeto, desde a definição inicial até a execução do plano de ação
         </p>
       </div>
       
--- agencia/src/componentes/Footer.jsx
+++ agencia/src/componentes/Footer.jsx
@@ -1,12 +1,60 @@
 import '../styles/componentes/Footer.sass'
+import Logo from '/image/logo.png'
+import Facebook from '/image/facebook.png'
+import Twitter from '/image/twitter.png'
+import Linkedin from '/image/linkedin.png'
+import Dribble from '/image/dribble.png'
+import Behance from '/image/behance.png'
+import Google from '/image/google-plus.png'
+
 
 function Footer() {
   
 
   return (
-    <footer className="footer__container">
-      <h1>footer</h1>
-        
+    <footer >
+      <div className="footer__container">
+        <img src={Logo} alt="Logo da Agencia M dourado" />
+        <p>
+            Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online ultilizando estratégias,
+            ferramentas e tecnologias personalizadas
+        </p>
+        <ul>
+          <li>
+            <a href="#">
+              <img src={Facebook} alt="logo facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Twitter} alt="logo Twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Linkedin} alt="logo linkedin" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Dribble} alt="logo dribble" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Behance} alt="logo behance" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Google} alt="logo google plus" />
+            </a>
+          </li>
+        </ul>
+        <p className='footer__copy'>
+          Copyright 2022 DS Pelizario
+        </p>
+      </div>
     </footer>
   )
 }
