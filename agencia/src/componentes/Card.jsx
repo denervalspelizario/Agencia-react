5aa09b60f8b3b923789b380cf9760435d1179b26
denervalspelizario <denervalspelizario@gmail.com>
2022-11-25 08:11:47 -0300

    update projeto agencia jsx section card

--- agencia/src/componentes/Card.jsx
+++ agencia/src/componentes/Card.jsx
@@ -5,7 +5,42 @@ function Card() {
 
   return (
     <section className="card__container">
-      <h1>card</h1>
+      <div className='card__cards'>
+        <p className='card__text--up'>
+          Junho 2012 -2016
+        </p>
+        <h3>Web Designer</h3>
+        <p className='card__text'>
+          Pied Piper StartUp
+        </p>
+        <p className='card__text'>
+          Criação de Landing pages, sites institucionais e E-comemerces completamente personalizados e otimizados para buscadores
+        </p>
+      </div>
+      <div className='card__cards'>
+        <p className='card__text--up'>
+          Junho 2012 -2016
+        </p>
+        <h3>Web Designer</h3>
+        <p className='card__text'>
+          Pied Piper StartUp
+        </p>
+        <p className='card__text'>
+          Criação de Landing pages, sites institucionais e E-comemerces completamente personalizados e otimizados para buscadores
+        </p>
+      </div>
+      <div className='card__cards'>
+        <p className='card__text--up'>
+          Junho 2012 -2016
+        </p>
+        <h3>Web Designer</h3>
+        <p className='card__text'>
+          Pied Piper StartUp
+        </p>
+        <p className='card__text'>
+          Criação de Landing pages, sites institucionais e E-comemerces completamente personalizados e otimizados para buscadores
+        </p>
+      </div>
       
         
     </section>
