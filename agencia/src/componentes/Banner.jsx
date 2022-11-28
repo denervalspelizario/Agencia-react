ec8d143a7fadb679a56dd2755aa64d1e0b1be73c
denervalspelizario <denervalspelizario@gmail.com>
2022-11-28 10:02:32 -0300

    update projeto agencia estilizacao e responsividade section banner

--- agencia/src/componentes/Banner.jsx
+++ agencia/src/componentes/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
       <img src={BannerImage} alt="Colaborador da Agencia"  title='Agencia de brading e design'/>
       <div className='banner__text'>
         <p>brading / ui / ux tecnologia</p>
-        <h1>Agência de Branding</h1>
-        <h1><span>e design digital</span></h1>
+        <h1><span>Agência de Branding</span></h1>
+        <h1>e design digital</h1>
       </div>
     </section>
   )
