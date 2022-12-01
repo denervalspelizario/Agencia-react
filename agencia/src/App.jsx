df36bf5a2bfa12348e66947329011b3b2d3ab064
denervalspelizario <denervalspelizario@gmail.com>
2022-12-01 11:02:19 -0300

    update projeto agencia impementaçao darkmode section header

--- agencia/src/App.jsx
+++ agencia/src/App.jsx
@@ -1,21 +1,24 @@
 
 import './styles/App.sass';
+import { useState } from 'react';
 import Header from './componentes/Header';
 import Banner from './componentes/Banner';
 import Experiencias from './componentes/Experiencias';
 import Footer from './componentes/Footer';
-import Theme from './componentes/Theme';
 
 function App() {
-  
+  const [ darkMode, setDarkmode ] = useState(false);
+
+    const alterTheme = () => {
+        setDarkmode(!darkMode)
+    }
 
   return (
-    <section className="App">
-      <Theme />
-      <Header />
-      <Banner />
-      <Experiencias />
-      <Footer />
+    <section className="App"> 
+      <Header  alterTheme={alterTheme} darkMode={darkMode} />
+      <Banner darkMode={darkMode}/>
+      <Experiencias darkMode={darkMode}/>
+      <Footer darkMode={darkMode}/>   
     </section>
   )
 }
--- agencia/src/componentes/Header.jsx
+++ agencia/src/componentes/Header.jsx
@@ -1,20 +1,27 @@
 import '../styles/componentes/Header.sass';
+import Moon from '/image/moon.png';
+import Sun from '/image/sun.png';
 import Logo from '/image/logo.png';
 
 
-function Header() {
+function Header(props) {
   return (
-    <header className="header">
-      <div className='header__container'>
+    <header className={props.darkMode ? 'header__dark__mode' : 'header__light__mode'}>
+      <div  className='button__container'>
+        <button onClick={props.alterTheme}  type='submit' className={props.darkMode ? 'dark__mode' : 'light__mode' }>
+          <img src={props.darkMode ? Sun : Moon} alt="" className='image__button'/>
+        </button>
+      </div>
+      <div id='header__container'  >
         <img src={Logo} alt="logo da agencia" className='header__logo'/>
         <nav>
           <ul>
-            <li><a href="#experiencias">Sobre</a></li>
-            <li><a href="#experiencias">Projetos</a></li>
-            <li><a href="#experiencias">Contato</a></li>
+            <li><a href="#experiencias" className={props.darkMode ? 'dark__mode' : 'light__mode'}>Sobre</a></li>
+            <li><a href="#experiencias" className={props.darkMode ? 'dark__mode' : 'light__mode'}>Projetos</a></li>
+            <li><a href="#experiencias" className={props.darkMode ? 'dark__mode' : 'light__mode'}>Contato</a></li>
           </ul>
-        </nav>
-      </div>       
+        </nav> 
+      </div>
     </header>
   )
 };
